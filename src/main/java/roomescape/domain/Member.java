a5206793047935e4836b08f978ae79d437f80330
minzzun99 <skg06123@naver.com>
2026-05-19 20:00:02 +0900

    feat: Member, MemberRepository 추가

---
+++ src/main/java/roomescape/domain/Member.java
@@ -0,0 +1,72 @@
+package roomescape.domain;
+
+public class Member {
+    private static final int MAX_LENGTH = 50;
+
+    private final Long id;
+    private final String name;
+    private final String email;
+    private final String password;
+
+    public Member(Long id, String name, String email, String password) {
+        validateName(name);
+        validateEmail(email);
+        validatePassword(password);
+
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Member(String name, String email, String password) {
+        this(null, name, email, password);
+    }
+
+    private void validateName(String name) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("이름은 비어 있을 수 없습니다. 이름을 입력해주세요.");
+        }
+        if (name.length() > MAX_LENGTH) {
+            throw new IllegalArgumentException(
+                    String.format("이름은 %d자를 넘을 수 없습니다. %d자 이내로 입력해주세요.", MAX_LENGTH, MAX_LENGTH));
+        }
+    }
+
+    private void validateEmail(String email) {
+        if (email == null || email.isBlank()) {
+            throw new IllegalArgumentException("이메일은 비어 있을 수 없습니다. 이메일을 입력해주세요.");
+        }
+        if (email.length() > MAX_LENGTH) {
+            throw new IllegalArgumentException(
+                    String.format("이메일은 %d자를 넘을 수 없습니다. %d자 이내로 입력해주세요.", MAX_LENGTH, MAX_LENGTH));
+        }
+    }
+
+    private void validatePassword(String password) {
+        if (password == null || password.isBlank()) {
+            throw new IllegalArgumentException("비밀번호는 비어 있을 수 없습니다. 비밀번호를 입력해주세요.");
+        }
+        if (password.length() > MAX_LENGTH) {
+            throw new IllegalArgumentException(
+                    String.format("비밀번호는 %d자를 넘을 수 없습니다. %d자 이내로 입력해주세요.", MAX_LENGTH, MAX_LENGTH));
+        }
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
---
+++ src/main/java/roomescape/repository/MemberRepository.java
@@ -0,0 +1,39 @@
+package roomescape.repository;
+
+import java.util.List;
+import java.util.Optional;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+import roomescape.domain.Member;
+
+@Repository
+public class MemberRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public MemberRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public Optional<Member> findByEmail(String email) {
+        String sql = "SELECT id, name, email, password FROM member WHERE email = ?;";
+        List<Member> result = jdbcTemplate.query(sql, memberRowMapper, email);
+        return result.stream().findAny();
+    }
+
+    public Optional<Member> findById(Long memberId) {
+        String sql = "SELECT id, name, email, password FROM member WHERE id = ?;";
+        List<Member> result = jdbcTemplate.query(sql, memberRowMapper, memberId);
+        return result.stream().findAny();
+    }
+
+    private final RowMapper<Member> memberRowMapper = (resultSet, rowNum) -> {
+        return new Member(
+                resultSet.getLong("id"),
+                resultSet.getString("name"),
+                resultSet.getString("email"),
+                resultSet.getString("password"));
+    };
+
+}
