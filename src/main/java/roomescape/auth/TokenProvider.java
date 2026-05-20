beb50014e3d77539a5aa03debf6b0689ae18c2b4
minzzun99 <skg06123@naver.com>
2026-05-20 13:46:25 +0900

    feat: JWT 토큰 발급 기능 추가

---
+++ src/main/java/roomescape/auth/TokenProvider.java
@@ -0,0 +1,40 @@
+package roomescape.auth;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TokenProvider {
+    private static final String SECRET_KEY = "roomescape-secret-key123456789";
+    private static final long EXPIRATION_TIME = 1000L * 60 * 60;        // 1시간
+
+    private final Key key = Keys.hmacShaKeyFor(SECRET_KEY.getBytes(StandardCharsets.UTF_8));
+
+    public String createToken(Long memberId) {
+        Date now = new Date();
+        Date expiration = new Date(now.getTime() + EXPIRATION_TIME);
+
+        return Jwts.builder()
+                .claim("memberId", memberId)
+                .setIssuedAt(now)
+                .setExpiration(expiration)
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public Long extractMemberId(String token) {
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+        return claims.get("memberId", Long.class);
+    }
+}
