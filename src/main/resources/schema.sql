616a3e7ca8bddf45956c86daed562985aff57bbe
minzzun99 <skg06123@naver.com>
2026-05-21 12:19:15 +0900

    feat: 스키마 수정, store_manage 테이블 추가 및 data.sql 내부 데이터 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -10,8 +10,26 @@ CREATE TABLE IF NOT EXISTS member (
     name        VARCHAR(50)     NOT NULL,
     email       VARCHAR(50)    NOT NULL,
     password    VARCHAR(50)     NOT NULL,
+    role        VARCHAR(20)     NOT NULL,
     PRIMARY KEY (id),
     UNIQUE (email)
+);
+
+CREATE TABLE IF NOT EXISTS store (
+    id          BIGINT          NOT NULL AUTO_INCREMENT,
+    name        VARCHAR(100)    NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE (name)
+);
+
+CREATE TABLE IF NOT EXISTS store_manager (
+    id          BIGINT          NOT NULL AUTO_INCREMENT,
+    member_id   BIGINT          NOT NULL,
+    store_id    BIGINT          NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (member_id) REFERENCES member (id),
+    FOREIGN KEY (store_id) REFERENCES store (id),
+    UNIQUE (member_id, store_id)
 );
 
 CREATE TABLE IF NOT EXISTS theme (
@@ -26,14 +44,17 @@ CREATE TABLE IF NOT EXISTS theme (
 CREATE TABLE IF NOT EXISTS reservation (
     id      BIGINT       NOT NULL AUTO_INCREMENT,
     member_id BIGINT NOT NULL,
+    store_id BIGINT NOT NULL,
     date    DATE NOT NULL,
     time_id BIGINT NOT NULL,
     theme_id BIGINT NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (member_id) REFERENCES member (id),
+    FOREIGN KEY (store_id) REFERENCES store (id),
     FOREIGN KEY (time_id) REFERENCES reservation_time (id),
     FOREIGN KEY (theme_id) REFERENCES theme (id),
-    UNIQUE (date, time_id, theme_id)
+    UNIQUE (store_id, date, time_id, theme_id)
 );
 
 CREATE INDEX IF NOT EXISTS idx_reservation_member_id ON reservation (member_id);
+CREATE INDEX IF NOT EXISTS idx_reservation_store_id ON reservation (store_id);
